2752c91de87eada8ccd6870b5f1d64ded387a9eb
semiopteryx <semiopteryx@gmail.com>
2024-09-20 12:37:50 -0400

    jekyll config

--- js/main.js
+++ js/main.js
@@ -1,152 +1,237 @@
-// Row 1
-$("#turn-signal-left").append(i_turnSignalLeft);
-$(`label[for="engine"]`).append(i_engine);
-$(`label[for="battery"]`).append(i_battery);
-$(`label[for="brake-warning"]`).append(i_brakeWarning);
-$(`label[for="oil-pressure"]`).append(i_oilPressure);
-$(`label[for="door-ajar"]`).append(i_doorAjar);
-$(`label[for="defrost-front"]`).append(i_defrostFront);
-$(`label[for="defrost-rear"]`).append(i_defrostRear);
-$("#ac").append(i_ac1);
-$("#ac").append(i_ac2);
-$("#ac").append(i_ac3);
-$("#ac").append(i_ac4);
-$("#turn-signal-right").append(i_turnSignalRight);
+String.prototype.mySplit = function(separator, limit) {
+	var result = [];
+	var string = this;
+	var startIndex = 0;
+	var endIndex;
 
-// Row 2
-$("#headlights").append(i_lowBeam);
-$("#headlights").append(i_lowBeam);
-$("#headlights").append(i_highBeam);
-$("#headlights").append(i_fogLight);
-$(`label[for="tire-warning"]`).append(i_tireWarning);
-$(`label[for="traction-control"]`).append(i_tractionControl);
-$(`label[for="diesel-glow-plug"]`).append(i_dieselGlowPlug);
-$(`label[for="gas"]`).append(i_gas);
-$(`label[for="seatbelt"]`).append(i_seatbelt);
-$(`label[for="windshield"]`).append(i_windshield);
-$(`label[for="key"]`).append(i_key);
+	if (separator === '') {
+		for (var i = 0; i < string.length; i++) {
+			result.push(string.charAt(i));
+		}
+		return result;
+	}
+
+	if (separator === undefined) {
+		result.push(string);
+		return result;
+	}
+
+	while (true) {
+		endIndex = -1;
+		for (var i = startIndex; i < string.length; i++) {
+			if (string.charAt(i) === separator) {
+				endIndex = i;
+				break;
+			}
+		}
+
+		if (endIndex === -1) {
+			break;
+		}
+
+		result.push(string.substring(startIndex, endIndex));
+		startIndex = endIndex + 1;
+
+		if (limit !== undefined && result.length === limit) {
+			break;
+		}
+	}
+
+	result.push(string.substring(startIndex));
+	return result;
+}
+
+function getElementsByClass(context, className) {
+	context = context || document;
+	var elements = context.getElementsByTagName('*');
+	var result = [];
+
+	for (var i = 0; i < elements.length; i++) {
+		var classes = elements[i].className.mySplit(' ');
+		for (var j = 0; j < classes.length; j++) {
+			if (classes[j] === className) {
+				result.push(elements[i]);
+				break;
+			}
+		}
+	}
+
+	return result;
+}
+
+function addClass(el, cls) {
+	var filteredClassArray = [];
+	var classArray = el.className.mySplit(' ');
+	for (var i = 0; i < classArray.length; i++) {
+		if (classArray[i] !== '') filteredClassArray.push(classArray[i]);
+	}
+	filteredClassArray.push(cls);
+	el.className = filteredClassArray.join(' ');
+}
+
+function removeClass(el, cls) {
+	var filteredClassArray = [];
+	var classArray = el.className.mySplit(' ');
+	for (var i = 0; i < classArray.length; i++) {
+		if (classArray[i] !== '' && classArray[i] !== cls) filteredClassArray.push(classArray[i]);
+	}
+	el.className = filteredClassArray.join(' ');
+}
+
+function hasClass(el, cls) {
+	var filteredClassArray = [];
+	var classArray = el.className.mySplit(' ');
+	for (var i = 0; i < classArray.length; i++) {
+		if (classArray[i] !== '') filteredClassArray.push(classArray[i]);
+	}
+
+	var includes = false;
+	for (var i = 0; i < classArray.length; i++) {
+		if (filteredClassArray[i] == cls) includes = true;
+	}
+	return includes;
+}
 
 
 
 // Light events
-var turnSignalAudio = new Audio("audio/turn-signal.wav");
+/* var turnSignalAudio = new Audio('audio/turn-signal.wav');
 	turnSignalAudio.loop = true;
-	turnSignalAudio.preload = "auto";
+	turnSignalAudio.preload = 'auto';
 var isTurnSignalAudioPlaying = false;
-var hornAudio = new Audio("audio/horn.wav");
-	hornAudio.preload = "auto";
-var currentTurnSignal = "";
+var hornAudio = new Audio('audio/horn.wav');
+	hornAudio.preload = 'auto';
+var currentTurnSignal = ''; */
 
-$(".turn-signal").on("click", function() {
-	let direction = $(this).attr("id").replace("turn-signal-", "");
-	$(".turn-signal").attr("data-blinking", false);
+function turnSignalClick(turnSignal) {
+	/* var direction = turnSignal.id.replace('turn-signal-', '');
 
-	if (currentTurnSignal == "") {
+	if (currentTurnSignal == '') {
 		currentTurnSignal = direction;
 		isTurnSignalAudioPlaying = true;
 		turnSignalAudio.play();
-		$(this).attr("data-blinking", true);
 	} else if (currentTurnSignal == direction) {
-		currentTurnSignal = "";
+		currentTurnSignal = '';
 		isTurnSignalAudioPlaying = false;
 		turnSignalAudio.pause();
 		turnSignalAudio.currentTime = 0;
 	} else {
 		currentTurnSignal = direction;
-		$(this).attr("data-blinking", true);
+	} */
+}
+
+function lightClick(light) {
+	if (hasClass(light, 'light-on')) removeClass(light, 'light-on');
+	else addClass(light, 'light-on');
+}
+
+var headlightsIndex = 0;
+var headlights = document.getElementById('headlights');
+var headlightsLights = getElementsByClass(headlights, 'headlights-light');
+function headlightsClick() {
+	headlightsIndex = (headlightsIndex + 1) % 4;
+	var cycleCurrent = getElementsByClass(headlights, 'cycle-current')[0];
+	removeClass(cycleCurrent, 'cycle-current');
+	addClass(headlightsLights[headlightsIndex], 'cycle-current');
+}
+
+var acIndex = 0;
+var ac = document.getElementById('ac');
+var acLights = getElementsByClass(ac, 'ac-light');
+function acClick() {
+	acIndex = (acIndex + 1) % 4;
+	var cycleCurrent = getElementsByClass(ac, 'cycle-current')[0];
+	removeClass(cycleCurrent, 'cycle-current');
+	addClass(acLights[acIndex], 'cycle-current');
+}
+
+
+
+// Buttons
+var locked = false;
+var lockBtn = document.getElementById('lock-btn');
+var unlockBtn = document.getElementById('unlock-btn');
+var hazardLightBtn = document.getElementById('hazard-lights-btn');
+
+function lockClick() {
+	if (!locked) {
+		locked = true;
+		addClass(lockBtn, 'btn-pressed');
+		removeClass(unlockBtn, 'btn-pressed');
 	}
-});
+}
 
-var ac = 0;
-$("#ac").on("click", function() {
-	ac = (ac + 1) % 4;
-	$("#ac").children().css("opacity", 0);
-	$("#ac").children().eq(ac).css("opacity", 1);
-});
+function unlockClick() {
+	if (locked) {
+		locked = false;
+		removeClass(lockBtn, 'btn-pressed');
+		addClass(unlockBtn, 'btn-pressed');
+	}
+}
 
-var headlights = 0;
-$("#headlights").on("click", function() {
-	headlights = (headlights + 1) % 4;
-	$("#headlights").children().css("opacity", 0);
-	$("#headlights").children().eq(headlights).css("opacity", 1);
-});
+function hazardLightClick() {
+	if (hasClass(hazardLightBtn, 'btn-pressed')) {
+		/* currentTurnSignal = '';
+		isTurnSignalAudioPlaying = false;
+		turnSignalAudio.pause();
+		turnSignalAudio.currentTime = 0; */
+		removeClass(hazardLightBtn, 'btn-pressed');
+	} else {
+		/* currentTurnSignal = 'both';
+		isTurnSignalAudioPlaying = true;
+		turnSignalAudio.play(); */
+		addClass(hazardLightBtn, 'btn-pressed');
+	}
+}
 
 
 
 // Wheel
-var isDragging = false;
-var lastAngle = 0;
-var currentAngle = 0;
-var velocity = 0;
-var lastTime = 0;
+var wheel = document.getElementById('wheel');
+var wheelCenterX = wheel.offsetLeft + wheel.offsetWidth / 2;
+var wheelCenterY = wheel.offsetTop + wheel.offsetHeight / 2;
 
-function getAngle(cx, cy, ex, ey) {
-	let dy = ey - cy;
-	let dx = ex - cx;
-	let theta = Math.atan2(dy, dx);
-	return theta * (180 / Math.PI);
+var WheelDrag = {
+	isDragging: false,
+	angleDelta: 10,
+	restingAngle: 0,
+	mouseDownAngle: 0,
+	mouseMoveAngle: 0,
+	mouseUpAngle: 0
 }
 
-$("#wheel").on("mousedown", function(e) {
-	isDragging = true;
-	$(this).css("transition", "transform 0s linear");
-
-	let wheelOffset = $(this).offset();
-	let centerX = wheelOffset.left + $(this).width() / 2;
-	let centerY = wheelOffset.top + $(this).height() / 2;
-
-	lastAngle = getAngle(centerX, centerY, e.pageX, e.pageY);
-	lastTime = e.timeStamp;
-
-	$(document).on("mousemove", function(e) {
-		if (isDragging) {
-			let newAngle = getAngle(centerX, centerY, e.pageX, e.pageY);
-			let angleDifference = newAngle - lastAngle;
-			
-			let timeDifference = e.timeStamp - lastTime;
-			velocity = angleDifference / timeDifference;
-
-			currentAngle += angleDifference;
-			lastAngle = newAngle;
-			lastTime = e.timeStamp;
-			$("#wheel").css("transform", `rotate(${currentAngle}deg)`);
-		}
-	});
-});
-
-$(document).on("mouseup", function() {
-if (isDragging) {
-	isDragging = false;
-	$(document).off("mousemove");
-
-	let momentumDuration = 500;
-	let decay = 0.8;
-
-	let momentumTime = 0;
-	let momentumInterval = setInterval(function() {
-		if (momentumTime < momentumDuration) {
-			currentAngle += velocity * 16;
-			$("#wheel").css("transform", `rotate(${currentAngle}deg)`);
-			
-			velocity *= decay;
-			momentumTime += 16;
-		} else {
-			clearInterval(momentumInterval);
-		}
-	}, 16);
+function getAngle(x, y) {
+	return Math.atan2(y - wheelCenterX, x - wheelCenterY);
 }
-});
 
-$(".horn").on("click", function(e) {
+function wheelMouseDown(e) {
+	WheelDrag.isDragging = true;
+	WheelDrag.mouseDownAngle = getAngle(e.clientX, e.clientY);
+}
+function wheelMouseMove(e) {
+	if (WheelDrag.isDragging) {	
+		WheelDrag.mouseMoveAngle = getAngle(e.clientX, e.clientY);
+		WheelDrag.mouseUpAngle = WheelDrag.restingAngle - (WheelDrag.mouseMoveAngle - WheelDrag.mouseDownAngle);
+		var angleChangeDeg = WheelDrag.mouseUpAngle * (180 / Math.PI);
+
+		angleChangeDeg = angleChangeDeg % 360;
+		if (angleChangeDeg < 0) angleChangeDeg += 360;
+		angleChangeDeg = Math.round(angleChangeDeg / WheelDrag.angleDelta) * WheelDrag.angleDelta;
+		if (angleChangeDeg == 360) angleChangeDeg = 0;
+
+		var oldWheelActive = getElementsByClass(document, 'wheel-active')[0];
+		var newWheelActive = document.getElementById('wheel' + angleChangeDeg);
+
+		removeClass(oldWheelActive, 'wheel-active');
+		addClass(newWheelActive, 'wheel-active');
+	}
+}
+function wheelMouseUp() {
+	WheelDrag.isDragging = false;
+	WheelDrag.restingAngle = WheelDrag.mouseUpAngle;
+}
+
+function hornMouseDown(e) {
 	e.stopPropagation();
-	hornAudio.play();
-});
-
-
-
-/* On load */
-$(document).ready(function() {
-	$(`input[type="checkbox"]`).each(function() {
-		if (Math.random() < 1) $(this).prop("checked", true);
-	});
-});
+	// hornAudio.play();
+}
